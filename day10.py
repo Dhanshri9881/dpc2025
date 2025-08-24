5f3778abaffca85c54bb12cceb0b64f923bdcb84
Dhanshri9881 <dhanshrishivaji.rumane@matoshri.edu.in>
2025-08-24 22:03:09 +0530

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,18 @@
+def groupAnagrams(strs):
+    anagram_map={}
+    for word in strs:
+        key=''.join(sorted(word))
+        if key in anagram_map:
+            anagram_map[key].append(word)
+        else:
+           anagram_map[key]=[word]
+    return list(anagram_map.values())
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(groupAnagrams([""]))
+print(groupAnagrams(["a"]))
+print(groupAnagrams(["abc","bca","cab","xyz","zyx","yxz"]))
+print(groupAnagrams(["abc","daf","ghi"]))
+
+
+
