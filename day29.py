210d91c616e77e232b16415121d2e40189ba15bf
Dhanshri9881 <dhanshrishivaji.rumane@matoshri.edu.in>
2025-09-12 15:00:44 +0530

    Create day29.py

---
+++ day29.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    fib=[0]*(n+1)
+    fib[0]=0
+    fib[1]=1
+    for i in range(2,n+1):
+        fib[i]=fib[i-1]+fib[i-2]
+    return fib[n]
+print(fibonacci(5))
+print(fibonacci(10))
+print(fibonacci(0))
+print(fibonacci(1000))
